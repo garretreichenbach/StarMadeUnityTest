c756478031e645763ed13ea7a272c33c27eee96f
Garret Reichenbach <garretreichenbach@gmail.com>
2025-09-09 21:27:02 -0700

    Update ChunkBuilder.cs

--- Assets/Universe/Data/Chunk/ChunkBuilder.cs
+++ Assets/Universe/Data/Chunk/ChunkBuilder.cs
@@ -24,41 +24,49 @@ namespace Universe.Data.Chunk {
 			var faceDirs = new List<byte>();
 
 			int totalBlocks = ChunkSize * ChunkSize * ChunkSize;
-			for (int i = 0; i < totalBlocks; i++) {
+			for(int i = 0; i < totalBlocks; i++) {
 				var type = chunk.GetBlockType(i);
 				if (type == 0) continue;
-				_blockCount ++;
+				_blockCount++;
 				var posV3 = chunk.GetBlockPosition(i);
-				int x = (int)posV3.x; int y = (int)posV3.y; int z = (int)posV3.z;
+				int x = (int)posV3.x;
+				int y = (int)posV3.y;
+				int z = (int)posV3.z;
 				// Check 6 neighbors
 				// +X neighbor
-				if (x + 1 >= ChunkSize || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x + 1, y, z))) == 0) {
-					facePositions.Add(new float3(x,y,z)); faceDirs.Add(0);
+				if(x + 1 >= ChunkSize || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x + 1, y, z))) != type) {
+					facePositions.Add(new float3(x, y, z));
+					faceDirs.Add(0);
 				}
 				// -X
-				if (x - 1 < 0 || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x - 1, y, z))) == 0) {
-					facePositions.Add(new float3(x,y,z)); faceDirs.Add(1);
+				if(x - 1 < 0 || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x - 1, y, z))) != type) {
+					facePositions.Add(new float3(x, y, z));
+					faceDirs.Add(1);
 				}
 				// +Y
-				if (y + 1 >= ChunkSize || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x, y + 1, z))) == 0) {
-					facePositions.Add(new float3(x,y,z)); faceDirs.Add(2);
+				if(y + 1 >= ChunkSize || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x, y + 1, z))) != type) {
+					facePositions.Add(new float3(x, y, z));
+					faceDirs.Add(2);
 				}
 				// -Y
-				if (y - 1 < 0 || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x, y - 1, z))) == 0) {
-					facePositions.Add(new float3(x,y,z)); faceDirs.Add(3);
+				if(y - 1 < 0 || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x, y - 1, z))) != type) {
+					facePositions.Add(new float3(x, y, z));
+					faceDirs.Add(3);
 				}
 				// +Z
-				if (z + 1 >= ChunkSize || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x, y, z + 1))) == 0) {
-					facePositions.Add(new float3(x,y,z)); faceDirs.Add(4);
+				if(z + 1 >= ChunkSize || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x, y, z + 1))) != type) {
+					facePositions.Add(new float3(x, y, z));
+					faceDirs.Add(4);
 				}
 				// -Z
-				if (z - 1 < 0 || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x, y, z - 1))) == 0) {
-					facePositions.Add(new float3(x,y,z)); faceDirs.Add(5);
+				if(z - 1 < 0 || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x, y, z - 1))) != type) {
+					facePositions.Add(new float3(x, y, z));
+					faceDirs.Add(5);
 				}
 			}
 
 			int totalFaces = facePositions.Count;
-			if (totalFaces == 0) {
+			if(totalFaces == 0) {
 				var empty = new Mesh();
 				empty.SetVertices(new List<Vector3>(0));
 				empty.SetIndices(Array.Empty<int>(), MeshTopology.Triangles, 0, false);
@@ -75,7 +83,7 @@ namespace Universe.Data.Chunk {
 				FacePositions = new NativeArray<float3>(facePositions.Count, Allocator.TempJob, NativeArrayOptions.UninitializedMemory),
 				FaceDirs = new NativeArray<byte>(faceDirs.Count, Allocator.TempJob, NativeArrayOptions.UninitializedMemory)
 			};
-			for (int i = 0; i < facePositions.Count; i++) {
+			for(int i = 0; i < facePositions.Count; i++) {
 				buildJob.FacePositions[i] = facePositions[i];
 				buildJob.FaceDirs[i] = faceDirs[i];
 			}
@@ -110,25 +118,25 @@ namespace Universe.Data.Chunk {
 			_triangleCount += totalIndexCount / 3;
 		}
 
-		// [BurstCompile]
+		[BurstCompile] //Disable this if you need to debug stuff
 		struct BuildFacesJob : IJobParallelFor {
 			public Mesh.MeshData OutputMesh;
 			[ReadOnly] public NativeArray<float3> FacePositions;
 			[ReadOnly] public NativeArray<byte> FaceDirs;
-			
+
 			public void Execute(int index) {
 				int vStart = index * 4;
 				int tStart = index * 6;
 				float3 basePos = FacePositions[index];
 				byte dir = FaceDirs[index];
-				
+
 				var outputVerts = OutputMesh.GetVertexData<Vector3>();
 				var outputNormals = OutputMesh.GetVertexData<Vector3>(stream: 1);
 				var outputUVs = OutputMesh.GetVertexData<Vector2>(stream: 2);
 				var outputTris = OutputMesh.GetIndexData<int>();
-				
+
 				// Write quad vertices based on face dir
-				for (int corner = 0; corner < 4; corner++) {
+				for(int corner = 0; corner < 4; corner++) {
 					float3 offset = GetFaceVertex(dir, corner);
 					float3 v = basePos + offset;
 					outputVerts[vStart + corner] = new Vector3(v.x, v.y, v.z);
@@ -144,72 +152,85 @@ namespace Universe.Data.Chunk {
 				outputTris[tStart + 4] = vStart + 3;
 				outputTris[tStart + 5] = vStart + 2;
 			}
-			
+
 			static float3 GetFaceNormal(byte dir) {
 				switch (dir) {
-					case 0: return new float3(1,0,0);
-					case 1: return new float3(-1,0,0);
-					case 2: return new float3(0,1,0);
-					case 3: return new float3(0,-1,0);
-					case 4: return new float3(0,0,1);
-					default: return new float3(0,0,-1);
+					case 0: return new float3(1, 0, 0);
+
+					case 1: return new float3(-1, 0, 0);
+
+					case 2: return new float3(0, 1, 0);
+
+					case 3: return new float3(0, -1, 0);
+
+					case 4: return new float3(0, 0, 1);
+
+					default: return new float3(0, 0, -1);
 				}
 			}
-			
+
 			static Vector2 GetFaceUV(int corner) {
 				switch (corner) {
-					case 0: return new Vector2(0,0);
-					case 1: return new Vector2(1,0);
-					case 2: return new Vector2(1,1);
-					default: return new Vector2(0,1);
+					case 0: return new Vector2(0, 0);
+
+					case 1: return new Vector2(1, 0);
+
+					case 2: return new Vector2(1, 1);
+
+					default: return new Vector2(0, 1);
 				}
 			}
-			
+
 			static float3 GetFaceVertex(byte dir, int corner) {
 				// Returns local-space vertex offset for the given face and corner
 				// Matching ElementInfo layout
 				switch (dir) {
 					case 0: // +X
 						return corner switch {
-							0 => new float3(0.5f,-0.5f,-0.5f),
-							1 => new float3(0.5f,-0.5f,0.5f),
-							2 => new float3(0.5f,0.5f,0.5f),
-							_ => new float3(0.5f,0.5f,-0.5f)
+							0 => new float3(0.5f, -0.5f, -0.5f),
+							1 => new float3(0.5f, -0.5f, 0.5f),
+							2 => new float3(0.5f, 0.5f, 0.5f),
+							_ => new float3(0.5f, 0.5f, -0.5f)
 						};
+
 					case 1: // -X
 						return corner switch {
-							0 => new float3(-0.5f,-0.5f,0.5f),
-							1 => new float3(-0.5f,-0.5f,-0.5f),
-							2 => new float3(-0.5f,0.5f,-0.5f),
-							_ => new float3(-0.5f,0.5f,0.5f)
+							0 => new float3(-0.5f, -0.5f, 0.5f),
+							1 => new float3(-0.5f, -0.5f, -0.5f),
+							2 => new float3(-0.5f, 0.5f, -0.5f),
+							_ => new float3(-0.5f, 0.5f, 0.5f)
 						};
+
 					case 2: // +Y
 						return corner switch {
-							0 => new float3(-0.5f,0.5f,-0.5f),
-							1 => new float3(0.5f,0.5f,-0.5f),
-							2 => new float3(0.5f,0.5f,0.5f),
-							_ => new float3(-0.5f,0.5f,0.5f)
+							0 => new float3(-0.5f, 0.5f, -0.5f),
+							1 => new float3(0.5f, 0.5f, -0.5f),
+							2 => new float3(0.5f, 0.5f, 0.5f),
+							_ => new float3(-0.5f, 0.5f, 0.5f)
 						};
+
 					case 3: // -Y
 						return corner switch {
-							0 => new float3(-0.5f,-0.5f,0.5f),
-							1 => new float3(0.5f,-0.5f,0.5f),
-							2 => new float3(0.5f,-0.5f,-0.5f),
-							_ => new float3(-0.5f,-0.5f,-0.5f)
+							0 => new float3(-0.5f, -0.5f, 0.5f),
+							1 => new float3(0.5f, -0.5f, 0.5f),
+							2 => new float3(0.5f, -0.5f, -0.5f),
+							_ => new float3(-0.5f, -0.5f, -0.5f)
 						};
+
 					case 4: // +Z
 						return corner switch {
-							0 => new float3(-0.5f,-0.5f,0.5f),
-							1 => new float3(-0.5f,0.5f,0.5f),
-							2 => new float3(0.5f,0.5f,0.5f),
-							_ => new float3(0.5f,-0.5f,0.5f)
+							0 => new float3(-0.5f, -0.5f, 0.5f),
+							1 => new float3(-0.5f, 0.5f, 0.5f),
+							2 => new float3(0.5f, 0.5f, 0.5f),
+							_ => new float3(0.5f, -0.5f, 0.5f)
 						};
+
 					default: // 5: -Z
 						return corner switch {
-							0 => new float3(0.5f,-0.5f,-0.5f),
-							1 => new float3(0.5f,0.5f,-0.5f),
-							2 => new float3(-0.5f,0.5f,-0.5f),
-							_ => new float3(-0.5f,-0.5f,-0.5f)
+							0 => new float3(0.5f, -0.5f, -0.5f),
+							1 => new float3(0.5f, 0.5f, -0.5f),
+							2 => new float3(-0.5f, 0.5f, -0.5f),
+							_ => new float3(-0.5f, -0.5f, -0.5f)
 						};
 				}
 			}
