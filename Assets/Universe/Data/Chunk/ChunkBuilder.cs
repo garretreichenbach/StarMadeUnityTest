04b6b1cc019f70e22019571a1ae0c4ad33b642fe
Garret Reichenbach <garretreichenbach@gmail.com>
2025-09-09 21:02:13 -0700

    Update ChunkBuilder.cs

--- Assets/Universe/Data/Chunk/ChunkBuilder.cs
+++ Assets/Universe/Data/Chunk/ChunkBuilder.cs
@@ -18,33 +18,33 @@ namespace Universe.Data.Chunk {
 		static int _triangleCount;
 
 		public static void BuildChunk(IChunkData chunk, MeshFilter meshFilter, MeshRenderer renderer) {
-			var buildJob = new ProcessMeshDataJob();
 
-			var inputMeshes = new List<Mesh>();
-			var vertexStarts = new List<int>();
-			var triStarts = new List<int>();
-			var positions = new List<float3>();
-			int totalVertexCount = 0;
-			int totalIndexCount = 0;
+			var facePositions = new List<float3>();
+			var faceDirs = new List<byte>();
 
-			// Gather meshes for all non-empty blocks and build compact start-offset arrays
 			int totalBlocks = ChunkSize * ChunkSize * ChunkSize;
 			for (int i = 0; i < totalBlocks; i++) {
-				// A block is considered present if its TYPE is non-zero
-				var blockType = chunk.GetBlockType(i);
-				if (blockType == 0) continue;
-				var mesh = ElementInfo.GetMesh(blockType);
-				inputMeshes.Add(mesh);
-				vertexStarts.Add(totalVertexCount);
-				triStarts.Add(totalIndexCount);
-				var pos = chunk.GetBlockPosition(i);
-				positions.Add(new float3(pos.x, pos.y, pos.z));
-				totalVertexCount += mesh.vertexCount;
-				totalIndexCount += mesh.GetSubMesh(0).indexCount;
+				var type = chunk.GetBlockType(i);
+				if (type == 0) continue;
+				var posV3 = chunk.GetBlockPosition(i);
+				int x = (int)posV3.x; int y = (int)posV3.y; int z = (int)posV3.z;
+				// Check 6 neighbors
+				// +X neighbor
+				if (x + 1 >= ChunkSize || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x+1,y,z))) == 0) { facePositions.Add(new float3(x,y,z)); faceDirs.Add(0); }
+				// -X
+				if (x - 1 < 0 || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x-1,y,z))) == 0) { facePositions.Add(new float3(x,y,z)); faceDirs.Add(1); }
+				// +Y
+				if (y + 1 >= ChunkSize || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x,y+1,z))) == 0) { facePositions.Add(new float3(x,y,z)); faceDirs.Add(2); }
+				// -Y
+				if (y - 1 < 0 || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x,y-1,z))) == 0) { facePositions.Add(new float3(x,y,z)); faceDirs.Add(3); }
+				// +Z
+				if (z + 1 >= ChunkSize || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x,y,z+1))) == 0) { facePositions.Add(new float3(x,y,z)); faceDirs.Add(4); }
+				// -Z
+				if (z - 1 < 0 || chunk.GetBlockType(chunk.GetBlockIndex(new Vector3(x,y,z-1))) == 0) { facePositions.Add(new float3(x,y,z)); faceDirs.Add(5); }
 			}
 
-			// Early-out: no geometry
-			if (inputMeshes.Count == 0) {
+			int totalFaces = facePositions.Count;
+			if (totalFaces == 0) {
 				var empty = new Mesh();
 				empty.SetVertices(new List<Vector3>(0));
 				empty.SetIndices(Array.Empty<int>(), MeshTopology.Triangles, 0, false);
@@ -54,16 +54,18 @@ namespace Universe.Data.Chunk {
 				return;
 			}
 
-			buildJob.VertexStart = new NativeArray<int>(vertexStarts.Count, Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
-			buildJob.TriStart = new NativeArray<int>(triStarts.Count, Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
-			buildJob.Positions = new NativeArray<float3>(positions.Count, Allocator.TempJob, NativeArrayOptions.UninitializedMemory);
-			for (int i = 0; i < vertexStarts.Count; i++) {
-				buildJob.VertexStart[i] = vertexStarts[i];
-				buildJob.TriStart[i] = triStarts[i];
-				buildJob.Positions[i] = positions[i];
+			int totalVertexCount = totalFaces * 4;
+			int totalIndexCount = totalFaces * 6;
+
+			var buildJob = new BuildFacesJob {
+				FacePositions = new NativeArray<float3>(facePositions.Count, Allocator.TempJob, NativeArrayOptions.UninitializedMemory),
+				FaceDirs = new NativeArray<byte>(faceDirs.Count, Allocator.TempJob, NativeArrayOptions.UninitializedMemory)
+			};
+			for (int i = 0; i < facePositions.Count; i++) {
+				buildJob.FacePositions[i] = facePositions[i];
+				buildJob.FaceDirs[i] = faceDirs[i];
 			}
 
-			buildJob.MeshData = Mesh.AcquireReadOnlyMeshData(inputMeshes);
 			var outputMeshData = Mesh.AllocateWritableMeshData(1);
 			buildJob.OutputMesh = outputMeshData[0];
 			buildJob.OutputMesh.SetIndexBufferParams(totalIndexCount, IndexFormat.UInt32);
@@ -74,7 +76,7 @@ namespace Universe.Data.Chunk {
 				new VertexAttributeDescriptor(VertexAttribute.TexCoord0, dimension: 2, stream: 2)
 			);
 
-			var handle = buildJob.Schedule(inputMeshes.Count, 4);
+			var handle = buildJob.Schedule(totalFaces, 32);
 			var newMesh = new Mesh();
 			var sm = new SubMeshDescriptor(0, totalIndexCount) { firstVertex = 0, vertexCount = totalVertexCount };
 			handle.Complete();
@@ -82,10 +84,8 @@ namespace Universe.Data.Chunk {
 			buildJob.OutputMesh.subMeshCount = 1;
 			buildJob.OutputMesh.SetSubMesh(0, sm);
 			Mesh.ApplyAndDisposeWritableMeshData(outputMeshData, new[] { newMesh });
-			buildJob.MeshData.Dispose();
-			buildJob.VertexStart.Dispose();
-			buildJob.TriStart.Dispose();
-			buildJob.Positions.Dispose();
+			buildJob.FacePositions.Dispose();
+			buildJob.FaceDirs.Dispose();
 			newMesh.RecalculateBounds();
 			meshFilter.mesh = newMesh;
 			renderer.material = Resources.Load<Material>("ChunkMaterial");
@@ -97,73 +97,106 @@ namespace Universe.Data.Chunk {
 		}
 
 		// [BurstCompile]
-		struct ProcessMeshDataJob : IJobParallelFor {
-			[ReadOnly]
-			public Mesh.MeshDataArray MeshData;
+		struct BuildFacesJob : IJobParallelFor {
 			public Mesh.MeshData OutputMesh;
-			public NativeArray<int> VertexStart;
-			public NativeArray<int> TriStart;
-			public NativeArray<float3> Positions;
+			[ReadOnly] public NativeArray<float3> FacePositions;
+			[ReadOnly] public NativeArray<byte> FaceDirs;
 			
 			public void Execute(int index) {
-				var data = MeshData[index];
-				var vCount = data.vertexCount;
-				var vStart = VertexStart[index];
-				var offset = Positions[index];
-				
-				var verts = new NativeArray<float3>(vCount, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
-				data.GetVertices(verts.Reinterpret<Vector3>());
-				
-				// Normals may be missing on some meshes
-				bool hasNormals = data.HasVertexAttribute(VertexAttribute.Normal);
-				NativeArray<float3> normals = default;
-				if (hasNormals) {
-					normals = new NativeArray<float3>(vCount, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
-					data.GetNormals(normals.Reinterpret<Vector3>());
-				}
-				
-				// UV0 may be missing on some meshes
-				bool hasUV0 = data.HasVertexAttribute(VertexAttribute.TexCoord0);
-				NativeArray<float2> uvs = default;
-				if (hasUV0) {
-					uvs = new NativeArray<float2>(vCount, Allocator.Temp, NativeArrayOptions.UninitializedMemory);
-					data.GetUVs(0, uvs.Reinterpret<Vector2>());
-				}
+				int vStart = index * 4;
+				int tStart = index * 6;
+				float3 basePos = FacePositions[index];
+				byte dir = FaceDirs[index];
 				
 				var outputVerts = OutputMesh.GetVertexData<Vector3>();
 				var outputNormals = OutputMesh.GetVertexData<Vector3>(stream: 1);
 				var outputUVs = OutputMesh.GetVertexData<Vector2>(stream: 2);
-				
-				for(int i = 0; i < vCount; i++) {
-					var v = verts[i] + offset;
-					outputVerts[i + vStart] = new Vector3(v.x, v.y, v.z);
-					outputNormals[i + vStart] = hasNormals ? normals[i] : Vector3.up;
-					outputUVs[i + vStart] = hasUV0 ? uvs[i] : Vector2.zero;
-				}
-				
-				verts.Dispose();
-				if (normals.IsCreated) normals.Dispose();
-				if (uvs.IsCreated) uvs.Dispose();
-				
-				var tStart = TriStart[index];
-				var subMesh = data.GetSubMesh(0);
-				var tCount = subMesh.indexCount;
-				var inputIndexStart = subMesh.indexStart;
-				var baseVertex = subMesh.baseVertex;
 				var outputTris = OutputMesh.GetIndexData<int>();
-				if (data.indexFormat == IndexFormat.UInt16) {
-					var tris = data.GetIndexData<ushort>();
-					for(int i = 0; i < tCount; ++i) {
-						int idx = tris[i + inputIndexStart] + baseVertex;
-						outputTris[i + tStart] = vStart + idx;
-					}
+				
+				// Write quad vertices based on face dir
+				for (int corner = 0; corner < 4; corner++) {
+					float3 offset = GetFaceVertex(dir, corner);
+					float3 v = basePos + offset;
+					outputVerts[vStart + corner] = new Vector3(v.x, v.y, v.z);
+					float3 n = GetFaceNormal(dir);
+					outputNormals[vStart + corner] = new Vector3(n.x, n.y, n.z);
+					outputUVs[vStart + corner] = GetFaceUV(corner);
 				}
-				else {
-					var tris = data.GetIndexData<int>();
-					for(int i = 0; i < tCount; ++i) {
-						int idx = tris[i + inputIndexStart] + baseVertex;
-						outputTris[i + tStart] = vStart + idx;
-					}
+				// Triangles (0,2,1, 0,3,2) with base vStart
+				outputTris[tStart + 0] = vStart + 0;
+				outputTris[tStart + 1] = vStart + 2;
+				outputTris[tStart + 2] = vStart + 1;
+				outputTris[tStart + 3] = vStart + 0;
+				outputTris[tStart + 4] = vStart + 3;
+				outputTris[tStart + 5] = vStart + 2;
+			}
+			
+			static float3 GetFaceNormal(byte dir) {
+				switch (dir) {
+					case 0: return new float3(1,0,0);
+					case 1: return new float3(-1,0,0);
+					case 2: return new float3(0,1,0);
+					case 3: return new float3(0,-1,0);
+					case 4: return new float3(0,0,1);
+					default: return new float3(0,0,-1);
+				}
+			}
+			
+			static Vector2 GetFaceUV(int corner) {
+				switch (corner) {
+					case 0: return new Vector2(0,0);
+					case 1: return new Vector2(1,0);
+					case 2: return new Vector2(1,1);
+					default: return new Vector2(0,1);
+				}
+			}
+			
+			static float3 GetFaceVertex(byte dir, int corner) {
+				// Returns local-space vertex offset for the given face and corner
+				// Matching ElementInfo layout
+				switch (dir) {
+					case 0: // +X
+						return corner switch {
+							0 => new float3(0.5f,-0.5f,-0.5f),
+							1 => new float3(0.5f,-0.5f,0.5f),
+							2 => new float3(0.5f,0.5f,0.5f),
+							_ => new float3(0.5f,0.5f,-0.5f)
+						};
+					case 1: // -X
+						return corner switch {
+							0 => new float3(-0.5f,-0.5f,0.5f),
+							1 => new float3(-0.5f,-0.5f,-0.5f),
+							2 => new float3(-0.5f,0.5f,-0.5f),
+							_ => new float3(-0.5f,0.5f,0.5f)
+						};
+					case 2: // +Y
+						return corner switch {
+							0 => new float3(-0.5f,0.5f,-0.5f),
+							1 => new float3(0.5f,0.5f,-0.5f),
+							2 => new float3(0.5f,0.5f,0.5f),
+							_ => new float3(-0.5f,0.5f,0.5f)
+						};
+					case 3: // -Y
+						return corner switch {
+							0 => new float3(-0.5f,-0.5f,0.5f),
+							1 => new float3(0.5f,-0.5f,0.5f),
+							2 => new float3(0.5f,-0.5f,-0.5f),
+							_ => new float3(-0.5f,-0.5f,-0.5f)
+						};
+					case 4: // +Z
+						return corner switch {
+							0 => new float3(-0.5f,-0.5f,0.5f),
+							1 => new float3(-0.5f,0.5f,0.5f),
+							2 => new float3(0.5f,0.5f,0.5f),
+							_ => new float3(0.5f,-0.5f,0.5f)
+						};
+					default: // 5: -Z
+						return corner switch {
+							0 => new float3(0.5f,-0.5f,-0.5f),
+							1 => new float3(0.5f,0.5f,-0.5f),
+							2 => new float3(-0.5f,0.5f,-0.5f),
+							_ => new float3(-0.5f,-0.5f,-0.5f)
+						};
 				}
 			}
 		}
