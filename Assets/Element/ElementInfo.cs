c5c30a3e16bb07c40df04a02ffb9b6e68391b5a6
Garret Reichenbach <garretreichenbach@gmail.com>
2025-09-14 16:40:55 -0700

    a

--- Assets/Element/ElementInfo.cs
+++ Assets/Element/ElementInfo.cs
@@ -1,4 +1,173 @@
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
 namespace Element {
 	public struct ElementInfo {
+		//Todo: Port the rest of the fields from BlockConfig.xml
+		public short TypeId { get; private set; }
+		[XmlAttribute("icon")]
+		public int IconId { get; private set; }
+		[XmlAttribute("name")]
+		public string Name { get; private set; }
+		[XmlAttribute("textureId")]
+		public short[] TextureIds { get; set; }
+		[XmlAttribute("type")]
+		public string IdName { get; private set; }
+		[XmlElement("Consistence")]
+		public Recipe Consistence { get; set; }
+		[XmlElement("ChamberPrerequisites")]
+		public short[] ChamberPrerequisites { get; set; }
+		[XmlElement("ChamberMutuallyExclusive")]
+		public short[] ChamberMutuallyExclusive { get; set; }
+		[XmlElement("ChamberChildren")]
+		public short[] ChamberChildren { get; set; }
+		[XmlElement("LightSourceColor")]
+		public float[] LightSourceColor { get; set; }
+		[XmlElement("ArmorValue")]
+		public float ArmorValue { get; set; }
+		[XmlElement("EffectArmor")]
+		public EffectArmor EffectArmor { get; set; }
+		[XmlElement("Price")]
+		public long Price { get; set; }
+		[XmlElement("Description")]
+		public string Description { get; set; }
+		[XmlElement("BlockResourceType")]
+		public int BlockResourceType { get; set; }
+		[XmlElement("ProducedInFactory")]
+		public short ProducedInFactory { get; set; }
+		[XmlElement("BasicResourceFactory")]
+		public short BasicResourceFactory { get; set; }
+		[XmlElement("FactoryBakeTime")]
+		public float FactoryBakeTime { get; set; }
+		[XmlElement("InventoryGroup")]
+		public string InventoryGroup { get; set; }
+		[XmlElement("Animated")]
+		public bool Animated { get; set; }
+		[XmlElement("Transparency")]
+		public bool HasTransparency { get; set; }
+		[XmlElement("InShop")]
+		public bool InShop { get; set; }
+		[XmlElement("Orientation")]
+		public bool HasOrientation { get; set; }
+		[XmlElement("BlockComputerReference")]
+		public short BlockComputerReference { get; set; }
+		[XmlElement("Slab")]
+		public byte SlabType { get; set; }
+		[XmlElement("SlabIds")]
+		public short[] SlabIds { get; set; }
+		[XmlElement("StyleIds")]
+		public short[] StyleIds { get; set; }
+		[XmlElement("SourceReference")]
+		public short SourceReference { get; set; }
+		[XmlElement("GeneralChamber")]
+		public bool IsGeneralChamber { get; set; }
+		[XmlElement("ChamberCapacity")]
+		public float ChamberCapacity { get; set; }
+		[XmlElement("ChamberRoot")]
+		public short ChamberRoot { get; set; }
+		[XmlElement("ChamberParent")]
+		public short ChamberParent { get; set; }
+		[XmlElement("ChamberUpgradesTo")]
+		public short ChamberUpgradesTo { get; set; }
+		[XmlElement("ChamberPermission")]
+		public short ChamberPermission { get; set; }
+		[XmlElement("ChamberAppliesTo")]
+		public byte ChamberAppliesTo { get; set; }
+		[XmlElement("ReactorHp")]
+		public float ReactorHp { get; set; }
+		[XmlElement("ReactorGeneralIconIndex")]
+		public int ReactorIconIndex { get; set; }
+		[XmlElement("Enterable")]
+		public bool IsEnterable { get; set; }
+		[XmlElement("Mass")]
+		public float Mass { get; set; }
+		[XmlElement("Volume")]
+		public float Volume { get; set; }
+		[XmlElement("Hitpoints")]
+		public int HP { get; set; }
+		[XmlElement("Placable")]
+		public bool IsPlacable { get; set; }
+		[XmlElement("InRecipe")]
+		public bool IsInRecipe { get; set; }
+		[XmlElement("CanActivate")]
+		public bool CanActivate { get; set; }
+		[XmlElement("IndividualSides")]
+		public byte IndividualSides { get; set; }
+		[XmlElement("SideTexturesPointToOrientation")]
+		public bool SideTexturesPointToOrientation { get; set; }
+		[XmlElement("HasActivationTexture")]
+		public bool HasActivationTexture { get; set; }
+		[XmlElement("MainCombinationController")]
+		public bool IsMainCombinationController { get; set; }
+		[XmlElement("SupportCombinationController")]
+		public bool IsSupportCombinationController { get; set; }
+		[XmlElement("EffectCombinationController")]
+		public bool IsEffectCombinationController { get; set; }
+		[XmlElement("Beacon")]
+		public bool IsBeacon { get; set; }
+		[XmlElement("Physical")]
+		public bool IsPhysical { get; set; }
+		[XmlElement("BlockStyle")]
+		public byte BlockStyle { get; set; }
+		[XmlElement("LightSource")]
+		public bool IsLightSource { get; set; }
+		[XmlElement("Door")]
+		public bool IsDoor { get; set; }
+		[XmlElement("SensorInput")]
+		public bool IsSensorInput { get; set; }
+		[XmlElement("DrawLogicConnection")]
+		public bool DrawLogicConnection { get; set; }
+		[XmlElement("Deprecated")]
+		public bool IsDeprecated { get; set; }
+		[XmlElement("ExplosionAbsorbtion")]
+		public float ExplosionAbsorbtion { get; set; }
+		[XmlElement("OnlyDrawnInBuildMode")]
+		public bool OnlyDrawnInBuildMode { get; set; }
+		[XmlElement("LodActivationAnimationStyle")]
+		public byte LodActivationAnimationStyle { get; set; }
+		[XmlElement("SystemBlock")]
+		public bool IsSystemBlock { get; set; }
+		[XmlElement("LogicBlock")]
+		public bool IsLogicBlock { get; set; }
+		[XmlElement("LogicSignaledByRail")]
+		public bool IsLogicSignaledByRail { get; set; }
+		[XmlElement("LogicBlockButton")]
+		public bool IsLogicBlockButton { get; set; }
+	}
+
+	[XmlRoot]
+	public struct ElementCategory {
+		public string name;
+		public ElementCategory[] parents;
+		public List<ElementCategory> childCategories;
+		public List<ElementInfo> members;
+	}
+
+	[XmlRoot("Consistence")]
+	public struct Recipe {
+		[XmlElement("Item")]
+		public List<ItemStack> Items { get; set; }
+	}
+
+	[XmlRoot("Item")]
+	public struct ItemStack {
+		[XmlAttribute("count")]
+		public int Count { get; set; }
+		[XmlText]
+		public string Name { get; set; }
+
+		public ElementInfo GetInfo() {
+			return ElementMap.GetInfo(Name);
+		}
+	}
+
+	[XmlRoot("EffectArmor")]
+	public struct EffectArmor {
+		[XmlText(Type = typeof(float))]
+		public float Heat;
+		[XmlText(Type = typeof(float))]
+		public float Kinetic;
+		[XmlText(Type = typeof(float))]
+		public float EM;
 	}
 }
---
+++ Assets/Element/Elements.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
+using UnityEditor;
+using UnityEngine;
+
+namespace Element {
+	public class ElementMap : MonoBehaviour {
+		readonly string _blockTypesPath = Application.dataPath + "/Config/BlockTypes.json";
+		readonly string _blockConfigPath = Application.dataPath + "/Config/BlockConfig.xml";
+
+		public static ElementInfo[] AllElements;
+		static readonly Dictionary<string, ElementInfo> ElementNameLookup = new Dictionary<string, ElementInfo>();
+		bool _loadedTypes;
+		bool _loadedConfig;
+		ElementCategory _configRoot;
+
+		public static ElementInfo GetInfo(short id) {
+			return AllElements[id];
+		}
+
+		public static ElementInfo GetInfo(string name) {
+			return ElementNameLookup[name.ToLower().Trim()];
+		}
+
+		/**
+		* Loads the ElementTypes.json file from the default location, or the given byte array if one is provided.
+		*/
+		public void LoadElementTypes(byte[] data = null) {
+			data ??= System.IO.File.ReadAllBytes(_blockTypesPath);
+			string json = System.Text.Encoding.UTF8.GetString(data);
+			AllElements = JsonUtility.FromJson<ElementInfo[]>(json);
+			foreach(ElementInfo element in AllElements) {
+				ElementNameLookup[element.Name.ToLower().Trim()] = element;
+			}
+			_loadedTypes = true;
+		}
+
+		/**
+		* Writes the ElementTypes.json file to the default location, or the given byte array if one is provided.
+		*/
+		public void WriteElementTypes(byte[] data = null) {
+			if(!_loadedTypes) {
+				return;
+			}
+			string json = JsonUtility.ToJson(AllElements, true);
+			if(data == null) {
+				File.WriteAllText(_blockTypesPath, json);
+			} else {
+				byte[] bytes = System.Text.Encoding.UTF8.GetBytes(json);
+				if(bytes.Length <= data.Length) {
+					System.Buffer.BlockCopy(bytes, 0, data, 0, bytes.Length);
+				}
+			}
+		}
+
+		/**
+		* Loads the ElementConfig.xml file from the default location, or the given byte array if one is provided.
+		*/
+		public void LoadElementConfig(byte[] data = null) {
+			if(!_loadedTypes) {
+				LoadElementTypes();
+			}
+			XmlSerializer serializer = new XmlSerializer(typeof(ElementCategory), new XmlRootAttribute("Config"));
+			_configRoot = (ElementCategory) serializer.Deserialize(new MemoryStream(data ?? File.ReadAllBytes(_blockConfigPath)));
+			_loadedConfig = true;
+		}
+
+		/**
+		* Writes the ElementConfig.xml file to the default location, or the given byte array if one is provided.
+		*/
+		public void WriteElementConfig(byte[] data = null) {
+			if(!_loadedConfig) {
+				return;
+			}
+			XmlSerializer serializer = new XmlSerializer(typeof(ElementCategory), new XmlRootAttribute("Config"));
+			serializer.Serialize(new MemoryStream(data ?? File.ReadAllBytes(_blockConfigPath)), _configRoot);
+		}
+	}
+
+	public enum Elements {
+		None = 0
+	}
+
+	[CustomEditor(typeof(ElementMap), false)]
+	public class ElementsGUI : Editor {
+		public override void OnInspectorGUI() {
+			DrawDefaultInspector();
+			GUILayout.Label("Element Types", EditorStyles.boldLabel);
+			GUILayout.BeginHorizontal();
+			if(GUILayout.Button("Load Element Types")) {
+				(target as ElementMap)?.LoadElementConfig();
+			}
+			if(GUILayout.Button("Save Element Types")) {
+				(target as ElementMap)?.WriteElementConfig();
+			}
+			GUILayout.EndHorizontal();
+			GUILayout.Label("Element Config", EditorStyles.boldLabel);
+			GUILayout.BeginHorizontal();
+			if(GUILayout.Button("Load Element Config")) {
+				(target as ElementMap)?.LoadElementTypes();
+			}
+			if(GUILayout.Button("Save Element Config")) {
+				(target as ElementMap)?.WriteElementTypes();
+			}
+		}
+	}
+}
